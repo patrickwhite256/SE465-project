39c6004699422321e9a9120e8535c977d682cf95
Patrick White <patrickwhite.256@gmail.com>
2015-03-06 00:28:12 -0500

    Probably not necessary to move the readme

--- marking.sh
+++ marking.sh
@@ -1,4 +1,4 @@
-FILES="clean.sh Makefile.common pipair.mak README.md test1 test2 test3 timeout.sh verify.sh"
+FILES="clean.sh Makefile.common pipair.mak test1 test2 test3 timeout.sh verify.sh"
 cp -R $FILES pi
 cd pi
 ./verify.sh
